eb5e9561fd2c97a74590d517c456cbc05c463bb6
刘迎春 <mhyh@live.com>
2020-09-24 10:58:54 +0800

    暂停使用消息功能

--- src/Master.php
+++ src/Master.php
@@ -40,10 +40,10 @@ class Master
             $this->ppid = posix_getppid();
 
             // 多进程时才开启消息队列
-            if($maxWorkers >= 2)
-            {
-                $this->makeQueue($this->pgid);
-            }
+//            if($maxWorkers >= 2)
+//            {
+//                $this->makeQueue($this->pgid);
+//            }
 
         }else{
             $this->ppid = 0; // 无
@@ -118,7 +118,7 @@ class Master
                     $message = $worker($key, $pid, $ppid);
                     $data = json_encode(['key' => $key, 'message' => $message]);
                     //将一条消息加入消息队列
-                    msg_send($this->queue, 1, $data);
+//                    msg_send($this->queue, 1, $data);
                     exit($key);
 
                 });
@@ -140,7 +140,7 @@ class Master
 
             } while ($pid != -1);
 
-            $this->massage();
+//            $this->massage();
         }
     }
 
@@ -159,7 +159,7 @@ class Master
             unset($data);
         }
 
-        $this->deleteQueue();
+//        $this->deleteQueue();
     }
 
 
